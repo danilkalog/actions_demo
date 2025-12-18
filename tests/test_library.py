b183dc3720f15bb32edadca2fd6f563a09679834
danilkalog <pisma4dan4ik@gmail.com>
2025-12-18 18:40:20 +0300

    Update test_library.py

--- tests/test_library.py
+++ tests/test_library.py
@@ -3,7 +3,7 @@ import os
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
 
-from library import Book, PrintedBook, EBook, User, Librarian, Library
+from library import Book, PrintedBook, EBook, User, Library
 
 
 class TestBook:
